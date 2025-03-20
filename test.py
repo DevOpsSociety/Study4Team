ebb7bb7ba6dd1254dc4f88a5546c712f34655e46
taenam kim <ktn2674@naver.com>
2025-03-20 16:45:40 +0900

    python modified

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-print("Hello Study 4 Team")
+num = int(input())
+print(num * num)
+
+#print('직각삼각형 그리기\n')
+# leg = int(input('변의 길이: '))
+
+# for i in range(leg):
+#     print('* ' * (i + 1))
+
+# area = (leg ** 2) / 2
+# print('넓이:', area)
+
+# print('Right triangle\n')
+# leg = int(raw_input('leg: '))
+
+# for i in range(leg):
+#     print('* ' * (i + 1))
+
+# area = (leg ** 2) / 2.0
+# print('area:', area)
+
+# print("Hello Study 4 Team")
+
