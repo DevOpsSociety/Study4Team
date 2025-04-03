141228f9f5618d4c84a3a84660e295c70d98545c
yyyerimm1126 <061126yeye@gmail.com>
2025-04-03 16:21:59 +0900

    Add files via upload

--- hw250403.py
+++ hw250403.py
@@ -1,47 +1,47 @@
 #5번
-# menu_dict= {
-#     "금요일": "탕수육",
-#     "토요일": "유산슬",
-#     "일요일": "팔보채",
-# }
-# for day, menu in menu_dict.items():
-#     print(f"{day}:{menu}")
+menu_dict= {
+    "금요일": "탕수육",
+    "토요일": "유산슬",
+    "일요일": "팔보채",
+}
+for day, menu in menu_dict.items():
+    print(f"{day}:{menu}")
     
 
 
-# #4번
-# num = [10,20,30,40,50,60,70,80,90,100] #리스트 값 
-# list = num[2:7] #3번째부터 7번쨰 요소 추출
-# print(f"3번쨰 요소부터 7번쨰 요소 = {list}")
-# second_list =  list[1] # 3번째 요소부터 7번째 요소 중 2번째 요소 
-# print(f"3번쩨 요소부터 7번째 요소 중 2번째 요소 = {second_list}") 
+#4번
+num = [10,20,30,40,50,60,70,80,90,100] #리스트 값 
+list = num[2:7] #3번째부터 7번쨰 요소 추출
+print(f"3번쨰 요소부터 7번쨰 요소 = {list}")
+second_list =  list[1] # 3번째 요소부터 7번째 요소 중 2번째 요소 
+print(f"3번쩨 요소부터 7번째 요소 중 2번째 요소 = {second_list}") 
 
 
 
 #3번
-# s= "maple"
-# middle = len(s) //2  #가운데 문자의 인덱스를 계산 (문자의 '위치'. 0,1,2...)
-# middle_char = s[middle] #그 인덱스에 해당하는 문자를 얻음 ('문자'를 의미. m,a,p...)
-# print(f"{s}의 가운데 글자는 {middle_char}입니다.") 
+s= "maple"
+middle = len(s) //2  #가운데 문자의 인덱스를 계산 (문자의 '위치'. 0,1,2...)
+middle_char = s[middle] #그 인덱스에 해당하는 문자를 얻음 ('문자'를 의미. m,a,p...)
+print(f"{s}의 가운데 글자는 {middle_char}입니다.") 
 # #문자열 앞에 f"를 붙히면, 그 문자열 안에 {}르 사용하여 변수나 표현을 삽입할 수 있음.
 # #중괄호 안에 넣고싶은 변수를 작성하면 문자열에 자동삽입.
 
 
 
 #2번
-# car_numbers = input()
-# last_four = car_numbers[-4:]
-# print(f"{car_numbers}의 차량 번호 4자리는{last_four}입니다")
+car_numbers = input()
+last_four = car_numbers[-4:]
+print(f"{car_numbers}의 차량 번호 4자리는{last_four}입니다")
 
 
 #1번
-# student ="31025"
+student ="31025"
 
-# grade = student[0]
-# class_num = student[1:3]
-# num = student[3:]
+grade = student[0]
+class_num = student[1:3]
+num = student[3:]
 
-# print(f"{grade}학년 {class_num}반 {num}번")
+print(f"{grade}학년 {class_num}반 {num}번")
 
 
 
