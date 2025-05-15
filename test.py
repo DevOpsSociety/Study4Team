688c07b32cb7bdd68a3e63349308f56e39b12b35
taenam kim <ktn2674@naver.com>
2025-05-15 14:37:42 +0900

    tn

--- test.py
+++ test.py
@@ -1,27 +1,41 @@
-num = int(input())
-print(num * num)
+n1, n2 = input().split()
+a = int(n1)
+b = int(n2)
 
-#print('직각삼각형 그리기\n')
-# leg = int(input('변의 길이: '))
 
-# for i in range(leg):
-#     print('* ' * (i + 1))
+if(a<b):
+  print("<")
+elif(a==b):
+  print("==")
+else:
+  print(">")
 
-# area = (leg ** 2) / 2
-# print('넓이:', area)
+print(f'{a} + {b} = {a+b}')
 
-# print('Right triangle\n')
-# leg = int(raw_input('leg: '))
+# num = int(input())
+# print(num * num)
 
-# for i in range(leg):
-#     print('* ' * (i + 1))
+# #print('직각삼각형 그리기\n')
+# # leg = int(input('변의 길이: '))
 
-# area = (leg ** 2) / 2.0
-# print('area:', area)
+# # for i in range(leg):
+# #     print('* ' * (i + 1))
 
-# print("Hello Study 4 Team")
+# # area = (leg ** 2) / 2
+# # print('넓이:', area)
 
-print("김태남")
-print("김태남")
-print("김태남")
+# # print('Right triangle\n')
+# # leg = int(raw_input('leg: '))
+
+# # for i in range(leg):
+# #     print('* ' * (i + 1))
+
+# # area = (leg ** 2) / 2.0
+# # print('area:', area)
+
+# # print("Hello Study 4 Team")
+
+# print("김태남")
+# print("김태남")
+# print("김태남")
 
