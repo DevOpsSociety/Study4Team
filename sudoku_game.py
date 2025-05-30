5dc9254c32bea4560320ce0d378ad75383f8f3e5
kyin <o01071833128@gmail.com>
2025-05-30 12:53:36 +0900

    sudoku game

--- sudoku_game.py
+++ sudoku_game.py
@@ -14,12 +14,9 @@ def find_next_empty(puzzle):
     return None, None
 
 def is_valid(puzzle, guess, row, col):
-    row_vals= puzzle[row]
-    if guess in row_vals:
+    if guess in puzzle[row]:
         return False
-    
-    col_vals = [puzzle[i][col] for i in range(9)]
-    if guess in col_vals:
+    if guess in [puzzle[r][col] for r in range(9)]:
         return False
     
     row_start = (row // 3) * 3
@@ -61,10 +58,13 @@ def make_puzzle(board, empty_cells=40):
     cells = [(r, c) for r in range(9) for c in range(9)]
     random.shuffle(cells)
 
-    for i in range(empty_cells):
-        row, col = cells[i]
-        puzzle[row][col] = -1
-        return board
+    count = 0
+    for r, c in cells:
+        if count >= empty_cells:
+            break
+        puzzle[r][c] = -1
+        count += 1
+    return puzzle
 
 if __name__ == "__main__":
     full_board = generate_full_board()
@@ -72,4 +72,11 @@ if __name__ == "__main__":
 
     print("스도쿠 퍼즐:")
     pprint(puzzle)
+
+    input("\n 엔터를 누르면 정답을 보여줍니다.")
+
+    print("\n 정답:")
+    pprint(full_board)
+
+
 
